ff02271ad89386b8c4d58cf95ef1a3f865a87b51
= <=>
2023-09-07 16:36:33 +0530

    State variable declaration in class comp

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 const User = (props) => {
+  const [count1] = useState(1);
+  const [count2] = useState(2);
   const { name } = props;
   return (
     <div className="border border-gray-500 m-5 p-3">
       <h1 className="font-bold text-sm">Data from Functioncal Component</h1>
+      <h1>Count1 : {count1}</h1>
+      <h1>Count2 : {count2}</h1>
       <h1>Name: {name}</h1>
       <h1>Location: Bangalore</h1>
       <h1>Adarsha-React</h1>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,15 +3,22 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-
     console.log(props);
+
+    this.state = {
+      count1: 1,
+      count2: 2,
+    };
   }
   render() {
     const { name, location } = this.props;
+    const { count1 } = this.state;
 
     return (
       <div className="border border-gray-500 m-5 p-3">
         <h1 className="font-bold text-sm">Data from Class Component</h1>
+        <h1> count1 : {count1}</h1>
+        <h1> count2 : {this.state.count2}</h1>
         <h1>Name: {name}</h1>
         <h1>Location: {location}</h1>
         <h1>Adarsha-React</h1>
